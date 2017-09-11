b7cc3b75d2651d0ab9c5715d1f791853d701f64f
Scott Montague <SMontague29@gmail.com>
2017-09-10 23:28:32 -0600

    Initial upload of MVP

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Chophouse Cheddar Burger');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Monterey Melt');
+INSERT INTO burgers (burger_name) VALUES ('A1 Thick & Hearty');
